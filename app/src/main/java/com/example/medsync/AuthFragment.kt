9b9b3a91860b7b2aaa5bb0a49221d9e4f80968ad
Ovik7 <melkon-ovik@mail.ru>
2023-05-25 00:47:25 +0300

    25.05.2023_00:47

---
+++ app/src/main/java/com/example/medsync/AuthFragment.kt
@@ -0,0 +1,30 @@
+package com.example.medsync
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+
+class AuthFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button.setOnClickListener{ view : View ->
+            view.findNavController().navigate(R.id.action_authFragment_to_mainFragment)
+        }
+
+
+        val button = view.findViewById<Button>(R.id.button)
+        button.setOnClickListener(View.OnClickListener {
+            findNavController().navigate(R.id.action_authFragment_to_mainFragment)
+//            val i = Intent(this , MainMain::class.java)
+//            startActivity(i)
+        })
+    }
+}
--- app/src/main/java/com/example/medsync/MainActivity.kt
+++ app/src/main/java/com/example/medsync/MainActivity.kt
@@ -2,19 +2,32 @@ package com.example.medsync
 
 //import com.google.firebase.database.DatabaseReference
 //import com.google.firebase.database.FirebaseDatabase
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 //import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_main)
+        setContentView(R.layout.fragment_auth)
+
+
+    }
+    fun toastMe(view: View) {
+        val myToast = Toast.makeText(this, "Hello, Toast!" , Toast.LENGTH_SHORT)
+
+        myToast.show()
+    }
 //        val database = FirebaseDatabase.getInstance()
 //        val myRef = database.getReference("message")
 
 //        myRef.setValue("Привет")
-    }
+
 }
 //
 //fun writeHelloToFirebase() {
